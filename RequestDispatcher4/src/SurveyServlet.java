35733b3ad7723b9b6673d585f869793571ca267d
git <github>
2015-04-19 02:22:41 -0700

    removed unused files

--- RequestDispatcher4/src/SurveyServlet.java
+++ RequestDispatcher4/src/SurveyServlet.java
@@ -1,6 +1,6 @@
 import java.io.IOException;
-import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,14 +8,28 @@ import javax.servlet.http.HttpServletResponse;
 
 public class SurveyServlet extends HttpServlet {
 
+	private static final long serialVersionUID = -2073410410021800124L;
+
 	public void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
+		String servletPath = request.getServletPath();
 
-		String n = request.getParameter("userName");
-		out.print("Welcome " + n);
+		if ("/".equalsIgnoreCase(servletPath)) {
+			System.out.println("create");
+			RequestDispatcher rd = request.getRequestDispatcher("survey.html");
+			rd.include(request, response);
+		}
+		if ("/createsurvey".equalsIgnoreCase(servletPath)) {
+			System.out.println("create");
+			RequestDispatcher rd = request.getRequestDispatcher("survey.html");
+			rd.include(request, response);
+		}
+		if ("/submitsurvey".equalsIgnoreCase(servletPath)) {
+			System.out.println("submit");
+		}
+		if ("/displaysurvey".equalsIgnoreCase(servletPath)) {
+			System.out.println("display");
+		}
 	}
-
 }
